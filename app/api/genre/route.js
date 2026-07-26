3bf5bbe4e84e49b10082833f776009d3d19b5db9
Jafx2 <jafethsalgado26@gmail.com>
2026-07-26 01:33:19 -0600

    corregir ruta de import de supabaseAdmin

--- app/api/genre/route.js
+++ app/api/genre/route.js
@@ -1,4 +1,4 @@
-import { supabaseAdmin } from '../../../lib/supabaseAdmin'
+import { supabaseAdmin } from '../../lib/supabaseAdmin'
 
 const CLIENT_ID = process.env.SPOTIFY_CLIENT_ID
 const CLIENT_SECRET = process.env.SPOTIFY_CLIENT_SECRET
