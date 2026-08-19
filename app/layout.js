ac207c9352803728f618dcbb1700a4e704158e66
Jafx2 <jafethsalgado26@gmail.com>
2026-08-19 16:59:43 -0600

    configurar favicon, apple-touch-icon y web manifest con el ícono de Wax

--- app/layout.js
+++ app/layout.js
@@ -17,6 +17,15 @@ export const metadata = {
     type: 'website',
     locale: 'es_ES',
   },
+  icons: {
+    icon: [
+      { url: '/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
+      { url: '/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
+    ],
+    apple: '/apple-touch-icon.png',
+    shortcut: '/favicon.ico',
+  },
+  manifest: '/site.webmanifest',
 }
 
 export default function RootLayout({ children }) {
